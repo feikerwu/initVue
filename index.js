dc3de330ecc7e183165f40c79c14ee40a63a8b79
feikerwu <feikerwu@gmail.com>
2018-05-16 12:09:09 +0800

    npm link

---
+++ index.js
@@ -0,0 +1,17 @@
+#!user/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+
+
+const argv = process.argv
+
+const vueFile = path.resolve(__dirname, argv[1])
+
+const src = fs.createReadStream('./tempalte.vue')
+const dist = fs.createWriteStream(vueFile)
+
+src.pipe(dist)
+
+
+
