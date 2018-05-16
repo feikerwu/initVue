7a9f50e4338a06a9f01d95db68b3f31ee2f3a6f6
feikerwu <feikerwu@gmail.com>
2018-05-16 15:41:40 +0800

    tag0.1

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-#!user/bin/env node
+#!/usr/bin/env node
 
 const path = require('path')
 const fs = require('fs')
 
-
 const argv = process.argv
 
-const vueFile = path.resolve(__dirname, argv[1])
+const vueFile = path.resolve(__dirname, argv[2])
 
 const src = fs.createReadStream('./tempalte.vue')
 const dist = fs.createWriteStream(vueFile)
